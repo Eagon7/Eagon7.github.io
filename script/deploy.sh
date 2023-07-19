05a39a56bb664d33a6708ac7fbb3c4d4a73e4489
Eagon Ellington <gyhoshi@gmail.com>
2023-07-19 10:47:46 +0800

    updateTeam

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,27 +1,16 @@
-# /bin/bash
- 
-# 确保脚本抛出遇到的错误
+# 忽略错误
 set -e
- 
-# 重新打包组件库
- 
-pnpm build
- 
-# 打包生成静态文件
-pnpm docs:build
- 
-# 进入待发布的 dist/ 目录
+
+# 构建
+npm run docs:build
+
+# 进入待发布的目录
 cd docs/.vitepress/dist
- 
+
+
 git init
-git add .
+git add -A
 git commit -m 'deploy'
- 
-# 部署到 https://<username>.github.io/<repo>
-git push -f https://github.com/Eagon7/EagonBlog main:github-pages
- 
-# 提交所有代码到github
-cd ../../../
-git add .
-git cm -m 'update'
-git push
+
+
+cd -
